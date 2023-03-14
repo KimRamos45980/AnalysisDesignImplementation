15a9f7a2e316ffbdeabb76913032b7b6660beeff
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2023-03-14 13:06:31 -0700

    Created DataGridView for persons

--- DesignImplementation/Form1.Designer.cs
+++ DesignImplementation/Form1.Designer.cs
@@ -28,10 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.BtnCreatePerson = new System.Windows.Forms.Button();
-            this.BtnSortList = new System.Windows.Forms.Button();
             this.BtnEditPerson = new System.Windows.Forms.Button();
+            this.personBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.designImplementationContextBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridViewPerson = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.streetAddressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zipCodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.BtnDeletePerson = new System.Windows.Forms.Button();
+            this.BtnSortList = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.designImplementationContextBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPerson)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnCreatePerson
@@ -44,44 +61,141 @@
             this.BtnCreatePerson.UseVisualStyleBackColor = true;
             this.BtnCreatePerson.Click += new System.EventHandler(this.BtnCreatePerson_Click);
             // 
-            // BtnSortList
-            // 
-            this.BtnSortList.Location = new System.Drawing.Point(12, 180);
-            this.BtnSortList.Name = "BtnSortList";
-            this.BtnSortList.Size = new System.Drawing.Size(126, 50);
-            this.BtnSortList.TabIndex = 1;
-            this.BtnSortList.Text = "Sort List";
-            this.BtnSortList.UseVisualStyleBackColor = true;
-            // 
             // BtnEditPerson
             // 
-            this.BtnEditPerson.Location = new System.Drawing.Point(12, 68);
+            this.BtnEditPerson.Location = new System.Drawing.Point(144, 12);
             this.BtnEditPerson.Name = "BtnEditPerson";
             this.BtnEditPerson.Size = new System.Drawing.Size(126, 50);
             this.BtnEditPerson.TabIndex = 2;
-            this.BtnEditPerson.Text = "Edit Person";
+            this.BtnEditPerson.Text = "Save Changes";
             this.BtnEditPerson.UseVisualStyleBackColor = true;
+            this.BtnEditPerson.Click += new System.EventHandler(this.BtnEditPerson_Click);
+            // 
+            // personBindingSource
+            // 
+            this.personBindingSource.DataSource = typeof(DesignImplementation.Person);
+            // 
+            // personBindingSource1
+            // 
+            this.personBindingSource1.DataSource = typeof(DesignImplementation.Person);
+            // 
+            // designImplementationContextBindingSource
+            // 
+            this.designImplementationContextBindingSource.DataSource = typeof(DesignImplementation.DesignImplementationContext);
+            // 
+            // dataGridViewPerson
+            // 
+            this.dataGridViewPerson.AutoGenerateColumns = false;
+            this.dataGridViewPerson.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPerson.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.firstNameDataGridViewTextBoxColumn,
+            this.lastNameDataGridViewTextBoxColumn,
+            this.streetAddressDataGridViewTextBoxColumn,
+            this.cityDataGridViewTextBoxColumn,
+            this.stateDataGridViewTextBoxColumn,
+            this.zipCodeDataGridViewTextBoxColumn,
+            this.phoneNumberDataGridViewTextBoxColumn});
+            this.dataGridViewPerson.DataSource = this.personBindingSource;
+            this.dataGridViewPerson.Location = new System.Drawing.Point(12, 68);
+            this.dataGridViewPerson.Name = "dataGridViewPerson";
+            this.dataGridViewPerson.RowTemplate.Height = 25;
+            this.dataGridViewPerson.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewPerson.Size = new System.Drawing.Size(816, 424);
+            this.dataGridViewPerson.TabIndex = 4;
+            this.dataGridViewPerson.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewPerson_CellClick);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // firstNameDataGridViewTextBoxColumn
+            // 
+            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            this.firstNameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // lastNameDataGridViewTextBoxColumn
+            // 
+            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            this.lastNameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // streetAddressDataGridViewTextBoxColumn
+            // 
+            this.streetAddressDataGridViewTextBoxColumn.DataPropertyName = "StreetAddress";
+            this.streetAddressDataGridViewTextBoxColumn.HeaderText = "StreetAddress";
+            this.streetAddressDataGridViewTextBoxColumn.Name = "streetAddressDataGridViewTextBoxColumn";
+            this.streetAddressDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // cityDataGridViewTextBoxColumn
+            // 
+            this.cityDataGridViewTextBoxColumn.DataPropertyName = "City";
+            this.cityDataGridViewTextBoxColumn.HeaderText = "City";
+            this.cityDataGridViewTextBoxColumn.Name = "cityDataGridViewTextBoxColumn";
+            this.cityDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // stateDataGridViewTextBoxColumn
+            // 
+            this.stateDataGridViewTextBoxColumn.DataPropertyName = "State";
+            this.stateDataGridViewTextBoxColumn.HeaderText = "State";
+            this.stateDataGridViewTextBoxColumn.Name = "stateDataGridViewTextBoxColumn";
+            this.stateDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // zipCodeDataGridViewTextBoxColumn
+            // 
+            this.zipCodeDataGridViewTextBoxColumn.DataPropertyName = "ZipCode";
+            this.zipCodeDataGridViewTextBoxColumn.HeaderText = "ZipCode";
+            this.zipCodeDataGridViewTextBoxColumn.Name = "zipCodeDataGridViewTextBoxColumn";
+            this.zipCodeDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // phoneNumberDataGridViewTextBoxColumn
+            // 
+            this.phoneNumberDataGridViewTextBoxColumn.DataPropertyName = "PhoneNumber";
+            this.phoneNumberDataGridViewTextBoxColumn.HeaderText = "PhoneNumber";
+            this.phoneNumberDataGridViewTextBoxColumn.Name = "phoneNumberDataGridViewTextBoxColumn";
+            this.phoneNumberDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // BtnDeletePerson
             // 
-            this.BtnDeletePerson.Location = new System.Drawing.Point(12, 124);
+            this.BtnDeletePerson.Location = new System.Drawing.Point(276, 12);
             this.BtnDeletePerson.Name = "BtnDeletePerson";
             this.BtnDeletePerson.Size = new System.Drawing.Size(126, 50);
             this.BtnDeletePerson.TabIndex = 3;
             this.BtnDeletePerson.Text = "Delete Person";
             this.BtnDeletePerson.UseVisualStyleBackColor = true;
             // 
+            // BtnSortList
+            // 
+            this.BtnSortList.Location = new System.Drawing.Point(408, 12);
+            this.BtnSortList.Name = "BtnSortList";
+            this.BtnSortList.Size = new System.Drawing.Size(126, 50);
+            this.BtnSortList.TabIndex = 1;
+            this.BtnSortList.Text = "Sort List";
+            this.BtnSortList.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(840, 504);
+            this.Controls.Add(this.dataGridViewPerson);
             this.Controls.Add(this.BtnDeletePerson);
             this.Controls.Add(this.BtnEditPerson);
             this.Controls.Add(this.BtnSortList);
             this.Controls.Add(this.BtnCreatePerson);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.designImplementationContextBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPerson)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -89,8 +203,20 @@
         #endregion
 
         private Button BtnCreatePerson;
-        private Button BtnSortList;
         private Button BtnEditPerson;
+        private BindingSource personBindingSource;
+        private BindingSource personBindingSource1;
+        private BindingSource designImplementationContextBindingSource;
+        private DataGridView dataGridViewPerson;
         private Button BtnDeletePerson;
+        private Button BtnSortList;
+        private DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn streetAddressDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn cityDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn stateDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn zipCodeDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn phoneNumberDataGridViewTextBoxColumn;
     }
 }
--- DesignImplementation/Form1.cs
+++ DesignImplementation/Form1.cs
@@ -1,17 +1,67 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
 namespace DesignImplementation
 {
     public partial class Form1 : Form
     {
+
+        private DesignImplementationContext? DbContext;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            this.DbContext = new DesignImplementationContext();
+
+            this.DbContext.Database.EnsureCreated();
+
+            this.DbContext.Person.Load();
+
+            this.personBindingSource.DataSource = DbContext.Person.Local.ToBindingList();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            this.DbContext?.Dispose();
+            this.DbContext = null;
+        }
+
 
         private void BtnCreatePerson_Click(object sender, EventArgs e)
         {
             this.Hide();
             AddPerson addPersonForm = new();
             addPersonForm.ShowDialog();
+        }
+
+        public static Person person = new Person();
+
+        private void BtnEditPerson_Click(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewPerson_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //int row = dataGridViewPerson.CurrentRow.Index;
+            //person.Id = int.Parse(dataGridViewPerson[0, row].Value.ToString());
+            //person.FirstName = dataGridViewPerson[1, row].Value.ToString();
+            //person.LastName = dataGridViewPerson[2, row].Value.ToString();
+            //person.StreetAddress = dataGridViewPerson[3, row].Value.ToString();
+            //person.City = dataGridViewPerson[4, row].Value.ToString();
+            //person.State = dataGridViewPerson[5, row].Value.ToString();
+            //person.ZipCode = dataGridViewPerson[6, row].Value.ToString();
+            //person.PhoneNumber = dataGridViewPerson[7, row].Value.ToString();
         }
     }
 }
